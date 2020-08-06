c6fed9ba305a4a131a3804e642f5fe78d295e7db
jmei16 <67976097+jmei16@users.noreply.github.com>
2020-08-06 14:28:27 -0500

    Add files via upload

---
+++ Jia Ming_M_TabbedApp_Exercise/Jia Ming_M_TabbedApp_Exercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Jia Ming_M_TabbedApp_Exercise
+//
+//  Created by Jia Ming Mei on 8/6/20.
+//  Copyright © 2020 Jia Ming Mei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
